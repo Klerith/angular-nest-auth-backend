b347ceef54d0c4da177ea66baac877ced8919b1a
Fernando Herrera <fernando.herrera85@gmail.com>
2023-04-25 10:59:00 -0400

    Primer commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+
+  await app.listen( process.env.PORT ?? 3000);
+}
+bootstrap();
