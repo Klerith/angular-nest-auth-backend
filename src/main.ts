3a729175b0c75e80fde47614ef08706b4dcbe970
Fernando Herrera <fernando.herrera85@gmail.com>
2023-04-25 11:05:00 -0400

    Port log added

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,10 @@ async function bootstrap() {
     })
   );
 
+  const PORT = process.env.PORT ?? 3000;
+    
+  console.log(`App corriendo en puerto: ${ PORT } `)
 
-  await app.listen( process.env.PORT ?? 3000);
+  await app.listen( PORT );
 }
 bootstrap();
